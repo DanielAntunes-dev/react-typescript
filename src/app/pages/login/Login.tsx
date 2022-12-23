7eb36a11d07459995ca9a5ba1fdb6c95a65ce3fd
Daniel Junior <dap.junioruvv@gmail.com>
2022-12-23 15:10:39 -0300

    feat: add login page and form

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react"
+
 export const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleEntrar = () => {
+    console.log(email)
+    console.log(password);
+    
+    
+  }
+
   return (
-    <p>Dashboard</p>
+    <div>
+      <form action="">
+        <label htmlFor="">
+          <span>Email</span>
+          <input value={email} onChange={e => setEmail(e.target.value)} />
+        </label>
+
+        <label htmlFor="">
+          <span>Senha</span>
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+        </label>
+
+        <button type="button" onClick={handleEntrar}>
+          Entrar
+        </button>
+      </form>
+    </div>
   )
 }
